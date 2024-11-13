a97769dd98c3bfe223a045a0b5938d3227022e6b
peter pan <apppeterpan@gmail.com>
2024-11-13 17:15:53 +0800

    Initial Commit

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by SHIH-YING PAN on 2024/11/13.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
